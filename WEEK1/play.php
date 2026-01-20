ced2225b7772a6d9841489249c5c9c712ebee993
Crixa1is <brandon96120@gmail.com>
2026-01-20 15:17:01 +0800

    third commit

--- WEEK1/play.php
+++ WEEK1/play.php
@@ -2,4 +2,7 @@
 $name = "Brandon";
 echo "Hello World $name ";
 echo ' this is a full stack class ' .$name;
+echo "<h1>My name is Brandon Laman</h1>";
+echo "<h2>My PA name is Miss Athirah<h2>";
+echo "<h3>My department is JTMK</h3>";
 ?>
