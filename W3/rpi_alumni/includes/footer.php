593f45d897597b4bfbd274bcb9b181490e7fb684
Crixa1is <brandon96120@gmail.com>
2026-01-27 15:55:18 +0800

    rpi

--- W3/rpi_alumni/includes/footer.php
+++ W3/rpi_alumni/includes/footer.php
@@ -1,6 +1,14 @@
 <hr>
  <footer style="text-align: center;">
  <p>RMI Alumini Official Site</p>
+ <p>Rensselaer Polytechnic Institute</p>
+ <p>Office of Alumni Engagement</p>
+<p>Heffner Alumni House</p>
+<p>1301 Peoples Avenue</p>
+<p>Troy, NY 12180 USA</p>
+<p>(518) 276-6205</p>
+<p>alumni@rpi.edu</p>
+<p>Privacy Policy & Copyright</p>
  </footer>
 </body>
 </html>
--- W3/rpi_alumni/includes/header.php
+++ W3/rpi_alumni/includes/header.php
@@ -7,8 +7,19 @@
  <header>
  <h1>RPI Portal</h1>
  <nav>
- <a href="index.php">Home</a> |
- <a href="login.php">Login</a>
+ <nav class="navbar">
+    <div class="container">
+        <a href="" class="navbrand-brand">
+            <a href="index.php">Home</a> |
+            <a href="login.php">Login</a> |
+            <a href="#">Event</a> |
+            <a href="#">Give</a> |
+            <a href="#">Career Networking</a> |
+            <a href="#">Reunion & Homecoming</a> |
+        </a>
+    </div>
+ </nav>
+
  </nav>
  </header>
  <hr>
