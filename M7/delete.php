c1fb59a0b40081e9ccc621d2ac5e9ca1a8383e7d
Crixa1is <brandon96120@gmail.com>
2026-03-05 11:34:54 +0800

    Delete and Insert Success

--- M7/delete.php
+++ M7/delete.php
@@ -1,6 +1,18 @@
 <?php
 require_once("config/app_config.php");
     $maklumat = mysqli_query($conn,"SELECT users.id, username as pengguna, email, password ,name as peranan  FROM spmp.users join roles on users.role_id = roles.id;");
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        $userid = $_POST['user_id'];
+
+        $stmt = mysqli_prepare($conn,'DELETE FROM users WHERE id=?;');
+        mysqli_stmt_bind_param($stmt,'i', $userid);
+
+        if(mysqli_stmt_execute($stmt)) {
+            $mesej = "<p style = 'color:green'>User succesfully deleted</p>";
+    } else {
+        $mesej = "<p style = 'color:red'>User fail to be deleted</p>";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -26,6 +38,12 @@ require_once("config/app_config.php");
                 <td><?php echo $pengguna['pengguna']; ?></td>
                 <td><?php echo $pengguna['peranan']; ?></td>
                 <td><input type="submit" value="Padam" class="btn btn-danger"></td>
+                <td>
+                    <form action="" method = "POST">
+                        <input type="hidden" name="user_id" value="<?php echo $pengguna['id']; ?>">
+                        <input type="submit" value="Delete">
+                    </form>
+                </td>
                 </tr>
         <?php endwhile; ?>
     </table>
