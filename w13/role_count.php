7be615c34e67db10c24081f18f91dbdf80c28782
Crixa1is <brandon96120@gmail.com>
2026-04-14 15:46:20 +0800

    w13

---
+++ w13/role_count.php
@@ -0,0 +1,9 @@
+<?php
+    include 'db.php';
+
+    $sql = "SELECT COUNT(*) AS total FROM roles";
+    $result = mysqli_query($conn,$sql);
+    $row = mysqli_fetch_assoc($result);
+    echo $row['total'];
+
+?>
